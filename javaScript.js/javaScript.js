3bf8a3b9911ac39424a649298ebcf9557602dc29
yongama <yongamafayo@gmail.com>
2020-07-08 14:59:43 +0200

    first commit

--- javaScript.js/javaScript.js
+++ javaScript.js/javaScript.js
@@ -12,61 +12,63 @@ const smsCostSetting = document.querySelector(".smsTotalSettings");
 
 const totalCostElemThree = document.querySelector(".totalSettings");
 
-var smsCostVal = 0;
-var callCostVal = 0;
-var warnLevel = 0;
-var critLevel = 0;
+// var smsCostVal = 0;
+// var callCostVal = 0;
+// var warnLevel = 0;
+// var critLevel = 0;
 
-var smsCostTotal = 0;
-var callCostTotal = 0;
-var allCostTotal = 0;
+// var smsCostTotal = 0;
+// var callCostTotal = 0;
+// var allCostTotal = 0;
+
+var settingsInstance = BillWithSettings()
+
+function updated(){
+  settingsInstance.setCallCost(Number(callCost.value));
+  settingsInstance.setSmsCost(Number(smsCost.value));
+  settingsInstance.setWarningLevel(Number(warningLevel.value));
+  settingsInstance.setDangerLevel(Number(criticalLevel.value));
+  
+  styleTotal()
+} 
 
   function ButtonClicked(){
   var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-  if(allCostTotal < critLevel){
+  // if(allCostTotal < critLevel){
   
-    if (checkedRadioBtn){
-  var itemChecked = checkedRadioBtn.value;
-  
-    if(itemChecked === "call"){
-      callCostTotal += callCostVal;
-    }
-    else if (itemChecked === "sms"){
-      smsCostTotal += smsCostVal;
-      
-    }
-    }
-    callCostElem.innerHTML = callCostTotal.toFixed(2);
-  smsCostSetting.innerHTML = smsCostTotal.toFixed(2);
-      allCostTotal = callCostTotal + smsCostTotal
-  totalCostElemThree.innerHTML = allCostTotal.toFixed(2);
+   if (checkedRadioBtn){
+   var itemChecked = checkedRadioBtn.value;
+  // alert(itemChecked)
+       if(itemChecked === "call"){
+      settingsInstance.makeCall()
+      }
+      else if (itemChecked === "sms"){
+      settingsInstance.sendSms()      
+     }
+     
+    callCostElem.innerHTML = settingsInstance.getTotalCallCost().toFixed(2);
+  smsCostSetting.innerHTML = settingsInstance.getTotalSmsCost().toFixed(2);
+      // allCostTotal = callCostTotal + smsCostTotal
+  totalCostElemThree.innerHTML = settingsInstance.getTotalCost().toFixed(2);
      
       styleTotal()
   }
 }
   
-function updated(){
-  callCostVal = Number(callCost.value);
-  smsCostVal = Number(smsCost.value);
-  warnLevel = Number(warningLevel.value);
-  critLevel = Number(criticalLevel.value);
-  
-  styleTotal()
-} 
 
 function styleTotal(){
   //const currentTotal = Number(roundedBillTotal);
   totalCostElemThree.classList.remove("danger");
   totalCostElemThree.classList.remove("warning");
-  
- if(allCostTotal >= warnLevel && allCostTotal <critLevel){
-   totalCostElemThree.classList.remove("danger");
-   totalCostElemThree.classList.add("warning");
- }
-  else if(allCostTotal >= critLevel){
-    totalCostElemThree.classList.remove("warning");
-    totalCostElemThree.classList.add("danger");   
-  }
+  totalCostElemThree.classList.add(settingsInstance.totalClassName())
+//  if(allCostTotal >= warnLevel && allCostTotal <critLevel){
+//    totalCostElemThree.classList.remove("danger");
+//    totalCostElemThree.classList.add("warning");
+//  }
+//   else if(allCostTotal >= critLevel){
+//     totalCostElemThree.classList.remove("warning");
+//     totalCostElemThree.classList.add("danger");   
+  // }
 }
 
 // function clicked(){
